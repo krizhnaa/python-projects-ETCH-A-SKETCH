a1a0b387fc7f1c6aa2f28a542b962b923900de19
krizhnaa <krizhnaprem@gmail.com>
2023-11-12 20:09:49 +0530

    Implemented all the functionalities

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ miya.shape('arrow')
 
 def move_for():
     miya.forward(25)
-
+def move_back():
+    miya.back(25)
+def turn_left():
+    miya.left(10)
+def turn_right():
+    miya.right(10)
 
 screen = Screen()
 screen.onkey(key='w', fun=move_for)
+screen.onkey(key='s', fun=move_back)
+screen.onkey(key='a', fun=turn_left)
+screen.onkey(key='d', fun=turn_right)
 
 screen.listen()
 screen.exitonclick()
