4695a4948330c37201a7078af3caf5983c080c9a
krizhnaa <krizhnaprem@gmail.com>
2023-11-12 20:12:11 +0530

    When 'c' is pressed the screen is cleared

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ screen.onkey(key='w', fun=move_for)
 screen.onkey(key='s', fun=move_back)
 screen.onkey(key='a', fun=turn_left)
 screen.onkey(key='d', fun=turn_right)
+screen.onkey(key='c', fun=screen.clear)
 
 screen.listen()
 screen.exitonclick()
